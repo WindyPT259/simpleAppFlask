723b036a8199840c9fa29f47533195a543fc7ba4
Linh Ho <linh.ho@dynagility.com>
2023-02-24 13:46:13 +0700

    TK-1: configuration

--- src/routes/users_routes.py
+++ src/routes/users_routes.py
@@ -2,5 +2,4 @@ from flask import Blueprint
 from src.controllers.user_controller import get_list, home
 
 users_routes = Blueprint ("users_routes", __name__)
-users_routes.route("/")(home)
-users_routes.route("/user/list/", methods = ["POST"])(get_list)
+users_routes.route("/user/list/", methods = ["GET"])(get_list)

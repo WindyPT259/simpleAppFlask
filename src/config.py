20f8db10a5034adec5cad376c2580efb424126bf
ltpthao <ltpthao@tma.com.vn>
2023-02-27 18:28:16 +0700

    API getlist and inser user

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
-#Connect DB
+
+
+# Connect DB
 SQLALCHEMY_DATABASE_URI = 'mysql +pymysql://root:250997@localhost/sampleapp?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_POOL_TIMEOUT = 20
@@ -6,12 +8,12 @@ SQLALCHEMY_POOL_TIMEOUT = 20
 DEBUG = True
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
-CSRF_ENABLED     = True
+CSRF_ENABLED = True
 
 # Auto reload templates
 TEMPLATES_AUTO_RELOAD = True
 
-# signing the data. 
+# signing the data.
 CSRF_SESSION_KEY = "secret"
 
 # Secret key for signing cookies
